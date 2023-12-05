835083da9d0839856955912ff21d221e9a8bd64e
Zerodoom <zerodoom675@mail.ru>
2023-12-05 11:31:46 +0300

    Сompleted solution

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,10 +10,14 @@ repositories {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.9.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation ("org.testng:testng:7.1.0")
+    testImplementation ("junit:junit:4.13")
 }
 
 tasks.test {
     useJUnitPlatform()
+    useTestNG()
+    useJUnit()
 }
